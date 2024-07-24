49b17b6ab20ec33cce9d26feef4333c3a43e9db7
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-07-24 00:25:46 +0000

    avance de los ejercicios .concat

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 let myArrayNumber = [1,2,3,4,5]
-let myArrayString = ['lunes', 'martes', 'Miércoles', 'Jueves', 'viernes', 'sábado', 'Domingo']
+let myArrayString = ['lunes', 'martes', 'Miércoles', 'Jueves', 'viernes', 'sábado', 'domingo']
 let myArrayBoolean = [true, false]
 
 
+// let myMixedArray = myArrayNumber.concat(myArrayString);
+// // console.log(myMixedArray);
+
+// // let myMixedArray2 = (myArrayNumber.concat(myArrayString)).concat(myArrayBoolean);
+// // console.log(myMixedArray2);
+
+// let myMixedArray2 = myMixedArray.concat(myArrayBoolean);
+// console.log(myMixedArray2);
+
+let contieneFds = function(){
+    for(let i in myArrayString){
+        if (myArrayString[i].toLowerCase()=="sábado";
+)
+}
+"Semana Completa"+(contieneFds? Sip; Nop)
